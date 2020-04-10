5163922ea51c87db35607ea642f9fb2481cb29d6
Goel, Gaurav <g.goel@sap.com>
2020-04-10 14:54:08 +0530

    adding form

---
+++ contactform.php
@@ -0,0 +1,15 @@
+<?php
+
+      if (isset($_POST['submit'])){
+        $name = $_POST['name'];
+        $mailFrom = $_POST['email'];
+        $findus = $_POST['find-us'];
+        $message = $_POST['message'];
+
+
+        $mailto = "g.goel@sap.com";
+        $headers = "From : Website".$mailFrom;
+        $txt = "You have recieved an email from ".$name.".\n\n" .$message;
+        $subject = "mail from basil website"
+        mail($mailto,$subject,$txt,$headers );
+      }
